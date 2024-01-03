06a5d81b4a36680be62c95729dc00064f6e6d333
Heuristic Pedals <heuristicpedals@gmail.com>
2024-01-03 17:36:01 +0000

    feat: added basic arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // collect command line arguments - ignore idx 0 (binary name)
+    let args: Vec<String> = env::args().collect();
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    println!("Searching for {} in {}", query, file_path);
 }
