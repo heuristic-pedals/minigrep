9c03032231981a4e5982505f940661b5f091c7b1
Heuristic Pedals <heuristicpedals@gmail.com>
2024-01-03 17:52:33 +0000

    refactor: arg parsing to struct

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,25 @@ use std::fs;
 fn main() {
     // collect command line arguments - ignore idx 0 (binary name)
     let args: Vec<String> = env::args().collect();
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let config: Config = Config::new(&args);
 
-    println!("Searching for {} in {}", query, file_path);
+    println!("Searching for {} in {}", config.query, config.file_path);
 
-    let contents = fs::read_to_string(&file_path).expect("Unable to read provided file path");
+    let contents: String =
+        fs::read_to_string(&config.file_path).expect("Unable to read provided file path");
     println!("With contents: {}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(parsed_args: &Vec<String>) -> Config {
+        Config {
+            query: parsed_args[1].clone(),
+            file_path: parsed_args[2].clone(),
+        }
+    }
 }
