722ad808febc212158e3af1c45f6b08fbcd1cc94
Heuristic Pedals <heuristicpedals@gmail.com>
2024-01-03 19:01:57 +0000

    feat/tests: added search function

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,28 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // use Box<dyn Error> to allow all error types to propagate
     let contents: String = fs::read_to_string(&config.file_path)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn single_result() {
+        let query = "duct";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\n";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
