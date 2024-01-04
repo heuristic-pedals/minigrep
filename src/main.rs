c1f8e8e764d5321009b9ac21b9cc04338a275134
Heuristic Pedals <heuristicpedals@gmail.com>
2024-01-04 20:35:46 +0000

    chore: revert "refactor: move file read to config build - improve testing"

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ fn main() {
     // collect command line arguments
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Application error: {}", err);
+        eprintln!("Error parsing arguments: {}", err);
         process::exit(1);
     });
 
     // run application
-    minigrep::run(config)
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application error: {err}");
+        process::exit(1);
+    };
 }
