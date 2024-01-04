4bcb8271df4599a79a17e529b4fd520a71869310
Heuristic Pedals <heuristicpedals@gmail.com>
2024-01-04 17:36:58 +0000

    feat: write error messages to std error

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     // collect command line arguments
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {}", err);
+        eprintln!("Error parsing arguments: {}", err);
         process::exit(1);
     });
 
     // run application
     if let Err(err) = minigrep::run(config) {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
     };
 }
